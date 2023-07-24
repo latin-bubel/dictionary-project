c4c48ced8fe75a8767a834da8008f9a457ca8449
latin bubel <esterapietkiewicz0110@gmail.com>
2023-07-24 16:39:35 +0200

    added Dictionary API integration
    added axios, shecodes dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    const apiKey = "cf2c0451b1a5ae2tod3f94f0b95d3fe6";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
