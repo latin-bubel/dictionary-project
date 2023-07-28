6c15da0154e951f018aaedbb18d6d68d2116b04b
latin bubel <esterapietkiewicz0110@gmail.com>
2023-07-28 16:01:20 +0200

    Included phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Result(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        <Phonetic phonetic={props.results.phonetic} />
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
